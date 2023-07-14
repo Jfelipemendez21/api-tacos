5f789b15785f1bc5a692e1ed36b57995f707e212
Jfelipemendez21 <jfelipemendez@ucundinamarca.edu.co>
2023-07-14 14:12:44 -0500

    Api v1.1!

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use("/api-tacos", rutas); 
 
 // PARA SERVIR LOS ARCHIVOS ESTATICOS EN ESTA DIRECCION 
-app.use("/public", express.static(`${__dirname}/imgs `))
+app.use("/public", express.static(`${__dirname}/imgs`))
 
 mongoose.connect(process.env.MONGODB_URI)
     .then(()=> console.log("Conexion a la base de datos exitosa!!"))
