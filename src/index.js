563206a844de940406cea1d1b0c89a750f4a30e6
Jfelipemendez21 <jfelipemendez@ucundinamarca.edu.co>
2023-07-17 11:26:35 -0500

    Api en despliegue

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 const express= require("express"); 
 const mongoose= require("mongoose");
-const cors= require("cors")
+// const cors= require("cors")
 require("dotenv").config(); 
 const rutas= require("./routes/rutas");
 
 const app= express(); 
 const port = process.env.PORT || 4200; 
 
-app.use(cors({
-    origin: '*'
-}));
+// app.use(cors({
+//     origin: '*'
+// }));
 
 app.use(express.json()); 
 app.use("/api-tacos", rutas); 
